57b0709d9909ca36fd9ec32482ef554380b3a091
zengjin07 <zengjin07@countrygarden.com.cn>
2019-11-15 17:29:01 +0800

    'second'

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor="#ff5777">
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="#ff5777">
+        <img slot="item-icon" src="~assets/img/tabbar/category.svg">
+        <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="#ff5777">
+        <img slot="item-icon" src="~assets/img/tabbar/cart.svg">
+        <img slot="item-icon-active" src="~assets/img/tabbar/cart_active.svg">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="#ff5777">
+        <img slot="item-icon" src="~assets/img/tabbar/profile.svg">
+        <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
   </div>
 </template>
 
+<script>
+  import TabBar from "./components/common/tabbar/TabBar";
+  import TabBarItem from "./components/common/tabbar/TabBarItem";
+  export default {
+    name: 'app',
+    components: {
+        TabBar,
+        TabBarItem
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  @import "assets/css/base.css";
 </style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,85 @@
+<template>
+    <div id="home">
+      <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+      <home-swiper :banners="banners"/>
+      <recommend-view :recommends="recommends"/>
+      <feature-view/>
+      <tab-control :titles="['流行','新款','精选']"></tab-control>
+
+    </div>
+</template>
+
+<script>
+    import HomeSwiper from "./childComps/HomeSwiper";
+    import RecommendView from "./childComps/RecommendView";
+    import FeatureView from "./childComps/FeatureView";
+
+    import NavBar from "components/common/navbar/NavBar";
+    import TabControl from "../../components/content/tabControl/TabControl";
+
+    import {getHomeMultidata,getHomeGoods} from "network/home";
+
+
+    export default {
+        name: "Home",
+        components: {
+            HomeSwiper,
+            RecommendView,
+            FeatureView,
+            NavBar,
+            TabControl
+        },
+        data() {
+          return {
+              banners:[],
+              recommends:[],
+              goods: {
+                  'pop': {page: 0, list: []},
+                  'new': {page: 0, list: []},
+                  'sell': {page: 0, list: []},
+              }
+          }
+        },
+        created() {
+            //1、请求多个数据
+            this.getHomeMultidata()
+
+            //2.请求商品数据
+            this.getHomeGoods('pop')
+            this.getHomeGoods('new')
+            this.getHomeGoods('sell')
+        },
+        methods: {
+            getHomeMultidata() {
+                getHomeMultidata().then(res =>{
+                    this.banners = res.data.banner.list;
+                    this.recommends = res.data.recommend.list;
+                })
+            },
+            getHomeGoods(type) {
+                const page = this.goods[type].page + 1
+                getHomeGoods(type, page).then(res =>{
+                    this.goods[type].list.push(...res.data.list)
+                    this.goods[type].page += 1
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+</style>
